ab48127c8f7367506ba33ec26c33121ebec5ce1e
KoshiNoLimit <zmsh04@gmail.com>
2019-10-25 20:55:49 +0300

    Scripted auto-commit on change (2019-10-25 20:55:49) by gitwatch.sh

--- src/main/java/AirWC.java
+++ src/main/java/AirWC.java
@@ -8,6 +8,8 @@ public class AirWC implements WritableComparable<AirWC> {
     private int id;
     private int ind;
 
+    public void init(){}
+
     AirWC(int id, int ind) {
         this.id = id;
         this.ind = ind;
