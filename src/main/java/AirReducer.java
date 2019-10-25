d65c787da7c8dc3314445057077641ae181a4658
KoshiNoLimit <zmsh04@gmail.com>
2019-10-25 21:12:29 +0300

    Scripted auto-commit on change (2019-10-25 21:12:29) by gitwatch.sh

--- src/main/java/AirReducer.java
+++ src/main/java/AirReducer.java
@@ -12,7 +12,7 @@ public class AirReducer extends Reducer<AirWC, Text, Text, Text> {
 
         String name = set.next().toString();
         if(set.hasNext()) {
-            int size = 0;
+            double size = 0.0;
             double min = 0.0, med = 0.0, max = 0.0;
             for (; set.hasNext(); size++) {
                 double x = Double.parseDouble(set.next().toString());
