8efddbeb57955089c029dfde52ca91cf9ac0efee
KoshiNoLimit <zmsh04@gmail.com>
2019-10-25 19:53:13 +0300

    Scripted auto-commit on change (2019-10-25 19:53:13) by gitwatch.sh

--- src/main/java/AirJoin.java
+++ src/main/java/AirJoin.java
@@ -1,27 +1,33 @@
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class AirJoin {
     public static void main(String[] args) throws Exception {
-        if(args.length !=2) {
+        if(args.length != 3) {
             System.err.println("args");
             System.exit(-1);
         }
+
         Job job = Job.getInstance();
         job.setJarByClass(AirJoin.class);
         job.setJobName("AirJoin");
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlyMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+        job.setMapOutputKeyClass(AirWC.class);
         job.setPartitionerClass(AirPartitioner.class);
         job.setGroupingComparatorClass(AirGC.class);
         job.setReducerClass(AirReducer.class);
-        job.setMapOutputKeyClass(AirWC.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
