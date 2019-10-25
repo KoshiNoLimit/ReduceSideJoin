21859e5460c089fb3c54dece22e9c52a14848bcd
KoshiNoLimit <zmsh04@gmail.com>
2019-10-25 16:00:10 +0300

    Scripted auto-commit on change (2019-10-25 16:00:10) by gitwatch.sh

--- src/main/java/AirReducer.java
+++ src/main/java/AirReducer.java
@@ -1,5 +1,6 @@
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class AirReducer extends Reducer {
+public class AirReducer extends Reducer <> {
 
 }
