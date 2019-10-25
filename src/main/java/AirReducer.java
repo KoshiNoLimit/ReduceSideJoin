71d344505cc6cda4cae996f27396e3279e973f01
KoshiNoLimit <zmsh04@gmail.com>
2019-10-25 18:03:04 +0300

    Scripted auto-commit on change (2019-10-25 18:03:04) by gitwatch.sh

--- src/main/java/AirReducer.java
+++ src/main/java/AirReducer.java
@@ -8,6 +8,6 @@ public class AirReducer extends Reducer<AirWC, Text, Text, Text> {
 
     @Override
     protected void reduce(AirWC key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-
+        Iterable<Text> set = values.iterator();
     }
 }
