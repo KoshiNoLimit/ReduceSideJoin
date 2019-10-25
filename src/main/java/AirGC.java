912669de607f448e46115d1fcaa281ae0763df82
KoshiNoLimit <zmsh04@gmail.com>
2019-10-25 15:45:46 +0300

    Scripted auto-commit on change (2019-10-25 15:45:46) by gitwatch.sh

--- src/main/java/AirGC.java
+++ src/main/java/AirGC.java
@@ -1,9 +1,9 @@
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
-public class AirGC extends WritableComparator {
+public class AirGC extends WritableComparator<AirWC> {
     @Override
-    public int compare(WritableComparable a, WritableComparable b) {
-        return a.hashCode() - b.hashCode();
+    public int compare(AirWC a, AirWC b) {
+        return a.getId() - b.getId();
     }
 }
