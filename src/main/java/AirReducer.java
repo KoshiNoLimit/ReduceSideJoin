10c8b8e0ccc598af8d9bdf24752364e8116d7c80
KoshiNoLimit <zmsh04@gmail.com>
2019-10-25 16:22:44 +0300

    Scripted auto-commit on change (2019-10-25 16:22:44) by gitwatch.sh

--- src/main/java/AirReducer.java
+++ src/main/java/AirReducer.java
@@ -1,7 +1,12 @@
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class AirReducer extends Reducer {
-    
+import java.io.IOException;
 
+public class AirReducer extends Reducer {
+
+    @Override
+    protected void reduce(AirWC key, Iterable values, Context context) throws IOException, InterruptedException {
+
+    }
 }
