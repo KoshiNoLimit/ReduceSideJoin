cd35873792a095fafc6b9eff772cb230d0400513
KoshiNoLimit <zmsh04@gmail.com>
2019-10-25 23:46:02 +0300

    Scripted auto-commit on change (2019-10-25 23:46:02) by gitwatch.sh

--- src/main/java/FlyMapper.java
+++ src/main/java/FlyMapper.java
@@ -7,19 +7,12 @@ import java.io.IOException;
 public class FlyMapper extends Mapper <LongWritable, Text, AirWC, Text> {
     private static final int ID = 14;
     private static final int DELAY = 18;
-    private static final int CANCEL = 19;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String[] set = value.toString().split(",");
-
-        if( (set[CANCEL].equals(("1.00"))) || (set[DELAY].equals("0.00"))) {
-            return;
+        String[] set = Parse.parse(value.toString(), 1);
+        if(set.length != 0) {
+            context.write(new AirWC(Integer.parseInt(set[ID]), 1), new Text(set[DELAY]));
         }
-
-        for (int i = 0; i < set.length; i++) {
-            set[i] = set[i].replaceAll("\"", "");
-        }
-        context.write(new AirWC(Integer.parseInt(set[ID]), 1), new Text(set[DELAY]));
     }
 }
