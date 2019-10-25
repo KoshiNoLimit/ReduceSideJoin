acdf54be56193b7f6108d0e4e20aae330ff7c17e
KoshiNoLimit <zmsh04@gmail.com>
2019-10-25 19:43:02 +0300

    Scripted auto-commit on change (2019-10-25 19:43:02) by gitwatch.sh

--- src/main/java/AirJoin.java
+++ src/main/java/AirJoin.java
@@ -1,3 +1,9 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class AirJoin {
     public static void main(String[] args) throws Exception {
         if(args.length !=2) {
@@ -5,16 +11,16 @@ public class AirJoin {
             System.exit(-1);
         }
         Job job = Job.getInstance();
-        job.setJarByClass(WordCountApp.class);
-        job.setJobName("Word count");
+        job.setJarByClass(AirJoin.class);
+        job.setJobName("AirJoin");
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        job.setMapperClass(WordMapper.class);
-        job.setReducerClass(WordReducer.class);
+        job.setPartitionerClass(AirPartitioner.class);
+        job.setGroupingComparatorClass(AirGC.class);
+        job.setReducerClass(AirReducer.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
-
     }
 }
