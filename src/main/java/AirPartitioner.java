958df518eadac39291d99e6509bda370093b7fdb
KoshiNoLimit <zmsh04@gmail.com>
2019-10-25 14:01:42 +0300

    Scripted auto-commit on change (2019-10-25 14:01:42) by gitwatch.sh

--- src/main/java/AirPartitioner.java
+++ src/main/java/AirPartitioner.java
@@ -1,4 +1,9 @@
 import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 
 public class AirPartitioner extends HashPartitioner {
+
+    @Override
+    public int getPartition(Object key, Object value, int numReduceTasks) {
+        
+    }
 }
