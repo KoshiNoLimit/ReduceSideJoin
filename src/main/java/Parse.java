65a908bbbfc5801d48c01c078eb6e772ad7d5ff2
KoshiNoLimit <zmsh04@gmail.com>
2019-10-25 23:32:07 +0300

    Scripted auto-commit on change (2019-10-25 23:32:07) by gitwatch.sh

--- src/main/java/Parse.java
+++ src/main/java/Parse.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Parse {
+    private static final Integer ID_F = 14;
+    private static final Integer DELAY = 18;
+    private static final Integer CANCEL = 19;
+    private static final Integer ID_A = 0;
+    private static final Integer NAME = 1;
+    private static final String
 }
