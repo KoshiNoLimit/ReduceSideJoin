cd391aa39ecc1180f49ed6e5c45a69e591c32706
KoshiNoLimit <zmsh04@gmail.com>
2019-10-26 12:40:00 +0300

    Scripted auto-commit on change (2019-10-26 12:40:00) by gitwatch.sh

--- src/main/java/Parse.java
+++ src/main/java/Parse.java
@@ -1,18 +1,22 @@
 class Parse {
     private static final Integer DELAY = 18;
     private static final Integer CANCEL = 19;
-    private static final String WALL = ",";
+    private static final String WALL_FLY = ",";
+    private static final String WALL_AIR = "\",\"";
     private static final String IS_CANCEL = "1.00";
     private static final String NOT_DELAY = "1.00";
     private static final String[] EMPTY = {};
     private static final String TRASH = "\"";
 
     static String[] parse(String s, Integer ind) {
-        String[] set = s.split(WALL);
+        String[] set;
         if(ind == 1) {
+            set = s.split(WALL_FLY);
             if(set[CANCEL].equals(IS_CANCEL) || set[DELAY].equals(NOT_DELAY)) {
                 return EMPTY;
             }
+        } else {
+            set = s.split(WALL_AIR);
         }
         for(int i = 0; i < set.length; i++) {
             set[i] = set[i].replaceAll(TRASH, "");
