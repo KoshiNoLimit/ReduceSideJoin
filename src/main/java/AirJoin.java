ff1adcf1daafa04807b13fc15b5569f0781021fc
KoshiNoLimit <zmsh04@gmail.com>
2019-10-26 12:28:20 +0300

    Scripted auto-commit on change (2019-10-26 12:28:20) by gitwatch.sh

--- src/main/java/AirJoin.java
+++ src/main/java/AirJoin.java
@@ -35,8 +35,8 @@ public class AirJoin {
 }
 //hadoop fs -rm -r -f /user/knl/output
 //mvn package
-//hadoop fs -copyFromLocal AIR.csv
-//hadoop fs -copyFromLocal TIME.csv
+//hadoop fs -copyFromLocal AIR1.csv
+//hadoop fs -copyFromLocal TIME1.csv
 //export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar
-//hadoop AirJoin AIR.csv TIME.csv output
+//hadoop AirJoin AIR1.csv TIME1.csv output
 //hadoop fs -copyToLocal output
