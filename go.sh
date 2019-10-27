7d0c63f5d4028fd4374db790225b7cd0476469b3
KoshiNoLimit <zmsh04@gmail.com>
2019-10-27 15:56:51 +0300

    Scripted auto-commit on change (2019-10-27 15:56:51) by gitwatch.sh

--- go.sh
+++ go.sh
@@ -2,8 +2,5 @@
 
 hadoop fs -rm -r -f /user/knl/output
 mvn package
-hadoop fs -copyFromLocal AIR1.csv
-hadoop fs -copyFromLocal TIME1.csv
-export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar
 hadoop AirJoin AIR1.csv TIME1.csv output
 hadoop fs -copyToLocal output
