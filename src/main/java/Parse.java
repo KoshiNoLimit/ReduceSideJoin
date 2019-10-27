7e02b6375de7069267d56ce1fae1d01a885f7890
KoshiNoLimit <zmsh04@gmail.com>
2019-10-27 13:06:24 +0300

    Scripted auto-commit on change (2019-10-27 13:06:24) by gitwatch.sh

--- src/main/java/Parse.java
+++ src/main/java/Parse.java
@@ -1,10 +1,12 @@
 class Parse {
     public static final Integer AIRPORT_ID = 0;
     public static final Integer FLY_ID = 14;
+    public static final Integer AIRPORT_IND = 0;
+    public static final Integer FLY_IND = 1;
     private static final Integer DELAY = 18;
     private static final Integer CANCEL = 19;
-    private static final String DELIMER_FLY = ",";
-    private static final String WALL_AIR = "\",\"";
+    private static final String DELIMITER_FLY = ",";
+    private static final String DELIMITER_AIR = "\",\"";
     private static final String IS_CANCEL = "1.00";
     private static final String NOT_DELAY = "1.00";
     public static final String[] EMPTY = {};
@@ -13,12 +15,12 @@ class Parse {
     static String[] parse(String s, Integer ind) {
         String[] set;
         if(ind == 1) {
-            set = s.split(WALL_FLY);
+            set = s.split(DELIMITER_FLY);
             if(set[CANCEL].equals(IS_CANCEL) || set[DELAY].equals(NOT_DELAY)) {
                 return EMPTY;
             }
         } else {
-            set = s.split(WALL_AIR);
+            set = s.split(DELIMITER_AIR);
         }
         for(int i = 0; i < set.length; i++) {
             set[i] = set[i].replaceAll(TRASH, "");
